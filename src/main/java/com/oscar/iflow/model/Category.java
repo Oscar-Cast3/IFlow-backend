21f4d60f77183a3438c31e975b2b0aeb2c7e93f8
Oscar-Cast3 <oscar.serrano@encora.com>
2025-08-13 20:19:27 -0600

    feat(validation): add validation annotations using Spring Valition

--- src/main/java/com/oscar/iflow/model/Category.java
+++ src/main/java/com/oscar/iflow/model/Category.java
@@ -1,5 +1,6 @@
 package com.oscar.iflow.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor @NoArgsConstructor
 public class Category {
     private Long id;
+    @NotBlank
     private String name;
 }
