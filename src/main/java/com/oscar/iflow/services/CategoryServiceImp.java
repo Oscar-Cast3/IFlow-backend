49f0121f9b9df7dab143d1bcbf9b79da28d570d2
Oscar-Cast3 <oscar.serrano@encora.com>
2025-08-14 21:09:42 -0600

    feat(service): add exceptions in category service

--- src/main/java/com/oscar/iflow/services/CategoryServiceImp.java
+++ src/main/java/com/oscar/iflow/services/CategoryServiceImp.java
@@ -2,7 +2,10 @@ package com.oscar.iflow.services;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
+import com.oscar.iflow.exceptions.APIException;
+import com.oscar.iflow.exceptions.ResourceNotFoundException;
 import com.oscar.iflow.model.Category;
 
 public class CategoryServiceImp implements CategoryService{
@@ -11,6 +14,16 @@ public class CategoryServiceImp implements CategoryService{
 
     @Override
     public Category createCategory(Category category) {
+        String normalizeName = category.getName().trim().toLowerCase();
+
+        boolean exists = storage.values().stream()
+                            .anyMatch(c -> c.getName().trim().toLowerCase().equals(normalizeName));
+
+        if (exists) {
+            throw new APIException("The category with the name: " + category.getName() + " already exists.");
+        }
+
+        category.setName(normalizeName);
         category.setId(idCounter++);
         storage.put(category.getId(), category);
         return category;
@@ -18,22 +31,42 @@ public class CategoryServiceImp implements CategoryService{
 
     @Override
     public Category getCategoryById(Long id) {
-        return storage.get(id);
+        return Optional.ofNullable(storage.get(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Category ", "id", id));
     }
 
     @Override
     public Map<Long, Category> getAllCategories() {
+        if(storage.isEmpty()) 
+            throw new APIException("No categories found");
+
         return storage;
     }
 
     @Override
     public Category updateCategory(Category category, Long id){
+        if (!storage.containsKey(id)) 
+            throw new ResourceNotFoundException("Category", "id", id);
+        
+        String normalizeName = category.getName().trim().toLowerCase();
+
+        boolean exists = storage.values().stream()
+                            .anyMatch(c -> !c.getId().equals(id) && c.getName().trim().toLowerCase().equals(normalizeName));
+
+        if (exists) {
+            throw new APIException("The category with the name: " + category.getName() + " already exists.");
+        }
+
+        category.setName(normalizeName);
         storage.replace(id, category);
         return storage.get(id);
     }
 
     @Override
     public Category deleteCategory(Long id) {
+        if (!storage.containsKey(id)) 
+            throw new ResourceNotFoundException("Category", "id", id);
+        
         Category category = storage.get(id);
         storage.remove(id);
         return category;
